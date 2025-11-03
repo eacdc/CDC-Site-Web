19aea7d0e650ce977ad7ea4985a57ce32ab8e94e
eacdc <ea@cdcprinters.com>
2025-11-03 16:41:41 +0530

    Initial commit - AHM GRN UI

--- script.js
+++ script.js
@@ -783,13 +783,13 @@
       });
       
       if (!isMachineMatch) {
-        // Show disabled View Status button if machine doesn't match
+        // Show enabled Start button if machine doesn't match - allows starting on different machine
         return `
-          <button class="btn-action btn-disabled" disabled>
+          <button class="btn-action btn-start" data-index="${processIndex}">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+              <path d="M8 5v14l11-7z"/>
             </svg>
-            View Status (Different Machine)
+            Start (different machine)
           </button>
         `;
       }
